59170086c08ae84a34d29bdaca270da4ef1fd828
Jorge Godoy <godoy.jf@gmail.com>
2023-04-27 14:02:21 -0400

    improve feedback delete

--- src/components/modals/Delete.vue
+++ src/components/modals/Delete.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+  import { ref } from 'vue'
   import Modal from './Modal.vue'
   import { type Video } from '../../types'
   import { store } from "../../store"
@@ -11,9 +12,13 @@
     (e: 'close'): void
   }>()
 
+  const status = ref("")
+
   async function handleRemove() {
+    status.value = "borrando"
     await store.removeVideo(props.video!.id);
     emit('close')
+    status.value = ""
   }
 
 </script>
@@ -21,7 +26,10 @@
 <template>
     <Teleport to="body">
         <Modal :show="!!video" @close="$emit('close')">
-            <div class="">
+            <div v-if="status == 'borrando'">
+              <p>borrando...</p>
+            </div>
+            <div v-else>
                 <h3 class="font-700 mb-2">¿Seguro que quieres eliminar este video?</h3>
                 <div class="flex justify-end mt-6">
                     <button class="b-1 b-blue c-blue px-4 rounded line-height-10" @click="$emit('close')">Cancelar</button>
