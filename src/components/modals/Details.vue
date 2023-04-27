25c4dd9c8c336ff20d96a20d6b4e560007248e81
Jorge Godoy <godoy.jf@gmail.com>
2023-04-27 09:54:00 -0400

    fix video details in mobile

--- src/components/modals/Details.vue
+++ src/components/modals/Details.vue
@@ -17,7 +17,7 @@
     <Teleport to="body">
         <Modal :show="!!video" @close="$emit('close')">
             <div class="flex flex-wrap">
-                <div class="bg-black flex-shrink-0 m-2" style="width:360px;height:206px">
+                <div class="video-container">
                     <iframe class="w-full h-full" :src="`https://www.youtube.com/embed/${video!.id}?controls=0`"></iframe>
                 </div>
                 <div class="flex-grow max-h-60 overflow-y-scroll m-2 flex-basis-100">
@@ -28,3 +28,11 @@
         </Modal>
     </Teleport>
 </template>
+
+<style scoped>
+.video-container {
+    @apply: bg-black flex-shrink-0 m-2 w-full;
+    max-width: 360px;
+    height: 206px;
+}
+</style>
